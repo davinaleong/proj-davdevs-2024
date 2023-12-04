125d869681904ee8da8ab76fbd84a2bdba3fe1b5
Davina Leong <davina.leong.sy@gmail.com>
2023-12-05 01:32:36 +0800

    Fixed errors

---
+++ src/scripts/config.ts
@@ -0,0 +1,19 @@
+import { logLoaded } from "./helpers"
+
+logLoaded(`config.ts`)
+
+export interface ConfigInterface {
+  appName: string
+  author: string
+  year: number
+  email: string
+  privacy: string
+}
+
+export const config: ConfigInterface = {
+  appName: "Dav/Devs",
+  author: "Davina Leong",
+  year: 2019,
+  email: "leong.shi.yun@gmail.com",
+  privacy: "1st Dec 2023",
+}
