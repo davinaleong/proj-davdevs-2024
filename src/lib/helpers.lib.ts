aeda5c3caf68c82c8f27dbd48eb577b7d356e502
Davina Leong <davina.leong.sy@gmail.com>
2024-01-10 12:06:37 +0800

    Fixed post ordering

--- src/lib/helpers.lib.ts
+++ src/lib/helpers.lib.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs"
 import { DATA_ACTIVE_ATTR, DATA_ELEMENT_ATTR, STR_TRUE } from "./strings.lib"
 
 // Interfaces
@@ -180,6 +181,76 @@ export function sortPostsDesc(array: any, key: string): any {
   return sorted.sort((a: any, b: any) => {
     const A = a.frontmatter[key]
     const B = b.frontmatter[key]
+
+    let comparison = 0
+    if (A > B) {
+      comparison = -1
+    } else if (A < B) {
+      comparison = 1
+    } else {
+      comparison = 0
+    }
+
+    return comparison
+  })
+}
+
+export function sortByDate(a: any, b: any) {
+  const A = dayjs(a.frontmatter.date).unix()
+  const B = dayjs(b.frontmatter.date).unix()
+
+  let comparison = 0
+  if (A > B) {
+    comparison = 1
+  } else if (A < B) {
+    comparison = -1
+  } else {
+    comparison = 0
+  }
+
+  return comparison
+}
+
+export function sortByDateDesc(a: any, b: any) {
+  const A = dayjs(a.frontmatter.date).unix()
+  const B = dayjs(b.frontmatter.date).unix()
+
+  let comparison = 0
+  if (A > B) {
+    comparison = -1
+  } else if (A < B) {
+    comparison = 1
+  } else {
+    comparison = 0
+  }
+
+  return comparison
+}
+
+export function sortPostsByDate(array: any): any {
+  const sorted: any = [...array]
+  return sorted.sort((a: any, b: any) => {
+    const A = dayjs(a.frontmatter.date).unix()
+    const B = dayjs(b.frontmatter.date).unix()
+
+    let comparison = 0
+    if (A > B) {
+      comparison = 1
+    } else if (A < B) {
+      comparison = -1
+    } else {
+      comparison = 0
+    }
+
+    return comparison
+  })
+}
+
+export function sortPostsByDateDesc(array: any): any {
+  const sorted: any = [...array]
+  return sorted.sort((a: any, b: any) => {
+    const A = dayjs(a.frontmatter.date).unix()
+    const B = dayjs(b.frontmatter.date).unix()
 
     let comparison = 0
     if (A > B) {
